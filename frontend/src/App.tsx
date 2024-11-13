17a86287eed5978076b43ff7270698efeb3f1ce9
jonathanhu237 <jonathanhu237@gmail.com>
2024-11-13 20:05:45 +0800

    feat: add file upload component with drag-and-drop support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,7 @@
-import { Input } from "./components/ui/input";
-import { Label } from "./components/ui/label";
+import UploadFile from "./components/upload-file";
 
 function App() {
-    return (
-        <div className="min-h-screen flex flex-col justify-center items-center">
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="picture">上传图片</Label>
-                <Input id="picture" type="file" />
-            </div>
-        </div>
-    );
+    return <UploadFile />;
 }
 
 export default App;
