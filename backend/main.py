e2875c29e0f4734691fb4b36712a1ca91a61753a
jonathanhu237 <jonathanhu237@gmail.com>
2024-11-13 21:34:38 +0800

    feat: add CORS middleware to backend for handling cross-origin requests

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 import cv2
 import numpy as np
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def calculate_average_spot_size(image_data):
